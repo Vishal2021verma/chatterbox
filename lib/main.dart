3275302ea7c0892ce59dd4274740e93d9d77ba42
Vishal2021verma <black2020panther@gmail.com>
2024-09-21 23:20:43 +0530

    firebse configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:chatterbox/firebase_options.dart';
 import 'package:chatterbox/screen/intro_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
